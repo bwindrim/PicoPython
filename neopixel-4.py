8584bb3281278a8af13c209d84f25bd6bc9464b8
Brian Windrim <bwindrim@gmail.com>
2022-12-21 20:39:17 +0000

    Removed default y arg to shader functions

--- neopixel-4.py
+++ neopixel-4.py
@@ -23,7 +23,7 @@ NEOPIXEL_PIN = 22 # GPIO22 on NeoPixel controller Pico Zero board
 LED_PIN = "LED"   # let MicroPython decide which pin controls the on-board LED
 
 g_gamma = 2.2
-g_brightness = 255.0*pow(1.0, g_gamma) # global brightness setting, range 0.0 - 1.0
+g_brightness = 255.0*pow(0.5, g_gamma) # global brightness setting, range 0.0 - 255.0
 
 # def hue_to_rgb(h):
 #     r =       abs(h * 6.0 - 3.0) - 1.0
@@ -59,28 +59,29 @@ def gamma_2(encoded):
             ceil(encoded[1]*encoded[1]*g_brightness),
             ceil(encoded[2]*encoded[2]*g_brightness))
 
-def shader_rgb1(time, x, y=0):
+def shader_rgb1(time, x, y):
     # treat time in seconds as degrees of angle
 #    assert(y == 0)
     angle = radians(time + x)*6.0 # map 60 seconds to full circle
     return ((1.0 + sin(angle*1.0))/2, (1.0 + sin(-angle*2.0))/2, (1.0 + sin(angle*3.0))/2)
 
-def shader_hsl1(time, x, y=0):
+def shader_hsl1(time, x, y):
     # hue ranges from 0 to 1 over the course of a minute, then wraps
     hue = (time + x) % 60.0 / 60.0
     return hsl_to_rgb(hue, 1.0, 0.1)
 
-def shader_hsl2(time, x, y=0):
+def shader_hsl2(time, x, y):
     # saturation ranges from 0 to 1 over the course of a minute, then wraps
     s = time % 60.0 / 60.0
     return hsl_to_rgb(2/3, s, x/59.0)
 
-def shader_hsl3(time, x, y=0):
+def shader_hsl3(time, x, y):
     # hue ranges from 0 to 1 over the course of a minute, then wraps
+#    print("x =", x, "y =", y)
     hue = (time*20 + x) % 15.0 / 15.0
-    return hsl_to_rgb(hue, y / 3, 0.5)
+    return hsl_to_rgb(hue, 1.0, (y + 1) / 8)
 
-def grayscale1(time, x, y=0): # grey ramp
+def grayscale1(time, x, y): # grey ramp
     return ((x / 59.0), (x / 59.0), (x / 59.0))
 
 def render(shader, gamma, time, width, height=1):
@@ -108,6 +109,6 @@ while True:
     if render_time > render_max:
         render_max = render_time
         print("render time =", render_time, "ms")
-    assert(render_time < 65)
+#    assert(render_time < 65)
    # time.sleep_ms(100 - render_time)
 
