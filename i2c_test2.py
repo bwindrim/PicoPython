469490ff71d3f6a2f328b04da1f1653f62efdcb7
Brian Windrim <bwindrim@gmail.com>
2023-02-07 21:54:41 +0000

    Working some of the time, with conditional printing

--- i2c_test2.py
+++ i2c_test2.py
@@ -16,26 +16,35 @@ Pin(27,  Pin.IN, Pin.PULL_UP) # enable internal pull-up resistor
 i2c_responder = I2CResponder(1, sda_gpio=26, scl_gpio=27, responder_address=0x41)
 
 status = 0x42
+do_prt = False
 
-def sleep(interval_ms):
+def sleep(interval_s):
     ""
-    time.sleep(1)
-    led.off()
-#    print("Sleeping for", interval_ms, "ms...")
-    picosleep.seconds(2)
     reason = 0 #wake_reason()
-    led.on()
-    time.sleep(1)
-#    print("...wakeup, reason =", reason)
+    if do_prt:
+        print("Sleeping for", interval_s, "s...")
+    time.sleep(1) # allow messages to get out before we stop the clocks
+    while interval_s > 0:
+        led.off()
+        if do_prt:
+            time.sleep(1)
+        else:
+            picosleep.seconds(2)
+        led.on()
+        time.sleep_ms(10)
+        interval_s -= 1
+    time.sleep(4)
+    if do_prt:
+        print("...wakeup, reason =", reason)
     return reason
 
 try:
     print("Polling I2C")
     prefix_reg = 0
-    # All times are in milliseconds
+    # All times are in milliseconds(?)
     ticks_base = time.ticks_ms()
     ticks_timeout = 5000
-    sleep_interval = 5000
+    sleep_interval = 5
     sleep(sleep_interval)
     while True:
         # Poll for I2C writes
@@ -45,41 +54,50 @@ try:
                 prefix_reg = buffer_in[0]   # first byte must be a register number
                 data = buffer_in[1:]        # copy the tail of the buffer
                 if data: # buffer tail wasn't empty
-                    print("Received I2C WRITE: reg =", prefix_reg, "data =", data, "len =", len(data))
+                    if do_prt:
+                        print("Received I2C WRITE: reg =", prefix_reg, "data =", data, "len =", len(data))
         # Poll for I2C reads                
         if i2c_responder.read_is_pending():
             if prefix_reg == 1: # status register + watchdog reset
                 ticks_base = time.ticks_ms() # reset watchdog time base
                 data = status.to_bytes(1, 'little')
                 assert(len(data) == 1)
-                print("Status")
+                if do_prt:
+                    print("Status")
             elif prefix_reg == 2: # ADC value
                 data = adc.read_u16().to_bytes(2,'little')
                 assert(len(data) == 2)
-                print("ADC")
+                if do_prt:
+                    print("ADC")
             elif prefix_reg == 3: # RTC date/time
                 data = pack("HBBBBBBH", *rtc.datetime())
                 assert(len(data) == 10)
-                print("RTC")
+                if do_prt:
+                    print("RTC")
             elif prefix_reg == 4: # status register read and clear
                 data = status.to_bytes(1, 'little')
                 assert(len(data) == 1)
                 status = 0
-                print("Status read and cleared")
+                if do_prt:
+                    print("Status read and cleared")
             else: # return a zero byte for all unrecognised regs
                 data = b'\x00'
                 assert(len(data) == 1)
-                print("Default")
+                if do_prt:
+                    print("Default")
             i2c_responder.put_read_bytes(data)
-            print("Sent I2C READ data for register", prefix_reg)
+            if do_prt:
+                print("Sent I2C READ data for register", prefix_reg)
             prefix_reg = 0
-            print()
+            if do_prt:
+                print()
         # Check for watchdog timeout
         ticks_now = time.ticks_ms()
         ticks_interval = time.ticks_diff(ticks_now, ticks_base)
         if ticks_timeout != 0 and ticks_interval >= ticks_timeout: # timeout set and reached
             # This is where we would cut the power to the Pi
-            print("Watchdog timeout exceeded: timeout =", ticks_timeout, "interval =", ticks_interval);
+            if do_prt:
+                print("Watchdog timeout exceeded: timeout =", ticks_timeout, "interval =", ticks_interval);
             #sleep(sleep_interval)
             ticks_base = ticks_now
             
