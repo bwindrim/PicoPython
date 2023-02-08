e0c293e007d1f1c705cdbd1f36592e0b934cc465
Brian Windrim <bwindrim@gmail.com>
2023-02-08 10:50:02 +0000

    Wake 40s/sleep 20s, I2C working during wake period

--- i2c_test2.py
+++ i2c_test2.py
@@ -43,9 +43,9 @@ try:
     prefix_reg = 0
     # All times are in milliseconds(?)
     ticks_base = time.ticks_ms()
-    ticks_timeout = 5000
-    sleep_interval = 5
-    sleep(sleep_interval)
+    ticks_timeout = 40000
+    sleep_interval = 20
+    sleep(1)
     while True:
         # Poll for I2C writes
         if i2c_responder.write_data_is_available():
@@ -98,8 +98,8 @@ try:
             # This is where we would cut the power to the Pi
             if do_prt:
                 print("Watchdog timeout exceeded: timeout =", ticks_timeout, "interval =", ticks_interval);
-            #sleep(sleep_interval)
-            ticks_base = ticks_now
+            sleep(sleep_interval)
+            ticks_base = time.ticks_ms()
             
 except KeyboardInterrupt:
     pass
