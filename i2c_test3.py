1f8c1b836840f760dbd180555d094c9ca870ff6a
Brian Windrim <bwindrim@gmail.com>
2023-02-20 21:46:40 +0000

    Changes from before break (for BirdBox1 preparation

--- i2c_test3.py
+++ i2c_test3.py
@@ -1,24 +1,25 @@
 ### i2s_test3.py
+import micropython
 import time
 import gc # garbage collector
+from micropython import const
 from machine import Pin, RTC, ADC, WDT, lightsleep
 from i2c_responder import I2CResponder
 from struct import pack, unpack
 
-btn = Pin(12, Pin.IN, Pin.PULL_UP)  # push-button input
-pwr = Pin(22, Pin.OUT, value=1)     # wide-input shim enable, high for power on
-led = Pin(25, Pin.OUT, value=1)     # Pico LED control
-sda = Pin(26,  Pin.IN, Pin.PULL_UP) # when using ADC pins for I2C...
-scl = Pin(27,  Pin.IN, Pin.PULL_UP) # ...we need to turn on the pull-ups
+micropython.opt_level(0) # zero is default, i.e. assertions are enabled
+
+btn = Pin(12, Pin.IN, Pin.PULL_UP) # push-button input
+pwr = Pin(22, Pin.OUT, value=1)    # wide-input shim enable, high for power on
+led = Pin(25, Pin.OUT, value=1)    # Pico LED control
+sda = Pin(26, Pin.IN, Pin.PULL_UP) # when using ADC pins for I2C...
+scl = Pin(27, Pin.IN, Pin.PULL_UP) # ...we need to turn on the pull-ups
 adc = ADC(Pin(28)) # main battery voltage measurement
 rtc = RTC()
 i2c = I2CResponder(1, sda_gpio=26, scl_gpio=27, responder_address=0x41)
 wdt = WDT(timeout=8388) # set ~8 sec (max) watchdog timeout
 
-status = 0x0
-watch_seconds = 30 # should be 4 minutes, in case Pi is hung when Pico restarts
-wake_seconds  = 30 # should be 15 minutes, so Pi doesn't stay off
-do_prt = False
+do_prt = True
 btn_down = False
 
 def pi_power_off():
@@ -41,10 +42,10 @@ def pi_power_on():
 
 def suspend(interval_s):
     "Sleep for the specified number of seconds, or until button pressed"
-    reason = 0x1
+    reason = 0x10
     if do_prt:
         print("Sleeping for", interval_s, "s...")
-    while interval_s > 0 and btn.value() is 1:
+    while interval_s > 0 and btn.value() is 1: # TBD: fix for interval_s % 5 != 0
         wdt.feed() # the watchdog timer appears to keep running during lightsleep()
         delay = min(interval_s*1000, 5000) # sleep for <= 5 seconds at a time
         if do_prt:
@@ -58,31 +59,33 @@ def suspend(interval_s):
         interval_s -= 5
     if interval_s > 0: # it was the button press that exited the loop
         btn_down = True
-        reason = 0x3 # button-press wakeup
+        reason = 0x30 # button-press wakeup
     if do_prt:
         print("...wakeup, reason =", reason)
     return reason
 
-# Check whether we were rebooted by the watchdog
-if machine.reset_cause() is machine.WDT_RESET:
+# Check whether we were rebooted by the watchdog (WDT)
+status = machine.reset_cause() # initialise status
+if status is machine.WDT_RESET:
     print("Watchdog reset")
-    status |= 0x20
 else:
-    assert(machine.reset_cause() is machine.PWRON_RESET)
+    assert(status is machine.PWRON_RESET)
     print("Power-on reset")
-    status |= 0x10
+watch_seconds = 30 # should be 4 minutes, in case Pi is hung when Pico restarts
+wake_seconds  = 30 # should be 15 minutes, so Pi doesn't stay off
 
 try:
     gc.disable() # we call garbage collection explicitly, below
     print("Polling I2C")
     prefix_reg = 0
     # All times are in milliseconds(?)
-    ticks_base = time.ticks_ms()
+    ticks_base = time.ticks_ms() # get the start time for the watch delay
 
     while True: # main loop
         wdt.feed()
+
         # Poll for I2C writes
-        if i2c.write_data_is_available():
+        if i2c.write_data_is_available(): # process register write
             buffer_in = i2c.get_write_bytes(max_size=16)
             if len(buffer_in) >= 1:         # received some data
                 prefix_reg = buffer_in[0]   # first byte must be a register number
@@ -98,7 +101,7 @@ try:
                     rtc.datetime(unpack("HBBBBBBH", data))
 
         # Poll for I2C reads                
-        if i2c.read_is_pending():
+        if i2c.read_is_pending(): # process register read
             if prefix_reg == 1: # status register + watch reset
                 ticks_base = time.ticks_ms() # reset watch time base
                 data = status.to_bytes(1, 'little')
@@ -136,22 +139,26 @@ try:
                 assert(len(data) == 1)
                 if do_prt:
                     print("Default")
-            i2c.put_read_bytes(data)
+            i2c.put_read_bytes(data) # send the register data
             if do_prt:
                 print("Sent I2C READ data for register", prefix_reg)
-            prefix_reg = 0
-            if do_prt:
-                print()
-        # Check for watch timeout (NOT watchdog)
+            prefix_reg = 0 # don't retain prefix
+
+        # Poll the pushbutton
+        if btn.value() is 0: # button is down
+            status |= 0x20 # set the button flag in the status
+
+        # Check for watch expiry (NOT the hardware watchdog)
         ticks_now = time.ticks_ms()
         ticks_interval = time.ticks_diff(ticks_now, ticks_base)
         if watch_seconds != 0 and ticks_interval >= watch_seconds*1000: # timeout set and reached
             if do_prt:
                 print("Watch timeout exceeded: timeout =", watch_seconds*1000, "interval =", ticks_interval);
             pi_power_off() # cut the power to the Pi
-            status |= suspend(wake_seconds) # set the status from the wakeup reason
+            status |= suspend(wake_seconds) # merge the wakeup reason into status
             pi_power_on() # restore power to the Pi
-            ticks_base = time.ticks_ms()
+            ticks_base = time.ticks_ms() # reset the watch
+
         # At the tail of the loop we give the garbage collector its own watchdog slice to run in
         wdt.feed()
         gc.collect()
