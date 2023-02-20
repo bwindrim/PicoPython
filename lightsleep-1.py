b6d2df3cf7e76d16ae6172d03ee2b753b89a78ef
Brian Windrim <bwindrim@gmail.com>
2023-02-20 22:02:14 +0000

    Modified lighsleep test

--- lightsleep-1.py
+++ lightsleep-1.py
@@ -1,7 +1,14 @@
+import machine
+import time
+
 count = 0
 hour_in_ms = 60 * 60 * 1000
+led = machine.Pin(25, machine.Pin.OUT, value=1)
 
 while True:
     print ("count =", count)
     count += 1
-    machine.lightsleep(hour_in_ms)
+    time.sleep_ms(500)
+    led.off()
+    machine.lightsleep(1)
+    led.on()
