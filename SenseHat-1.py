485bf03970d612d23ff083b46f929f9451c9190f
Brian Windrim <bwindrim@gmail.com>
2023-07-30 11:35:35 +0100

    Scrolling Hello World

--- SenseHat-1.py
+++ SenseHat-1.py
@@ -1,13 +1,10 @@
 from machine import I2C, SoftI2C, Pin
 import framebuf
+import time
 
 # ItsyBitsy RP2040 wires its SDA and SCL to GPIO2 and GPIO3 respectively, which are connected to I2C1
 i2c = I2C(1, scl=Pin(3), sda=Pin(2), freq=400000)
-
-#print (i2c.scan())
-fb = bytearray(128)
-buf = bytearray(192)
-fbuf = framebuf.FrameBuffer(fb, 8, 8, framebuf.RGB565)
+buf = bytearray(192) # space for 8x8 LED array, 3 bytes per LED
 
 def pixel(arr, x, y):
     "Extract a pixel x,y from the specified framebuffer bytearray and return it, with its co-ordinates"
@@ -16,13 +13,21 @@ def pixel(arr, x, y):
     hi=arr[n+1]
     return lo + (hi << 8), x, y
 
-def update(i2c, buff, fb, width=8):
+def create_framebuffer(width=8, height=8):
+    "Allocate a bytearray of the required size and then create a MicroPython framebuffer"
+    pixel_size = 2 # assume 16-bit pixels, for now
+    pix_format = framebuf.RGB565
+    fb = bytearray(width*height*pixel_size)
+    fbuf = framebuf.FrameBuffer(fb, width, height, pix_format)
+    return fbuf, fb
+
+def update(i2c, buff, fb, x_offset=0, y_offset=0, width=8):
     "Copy a MicroPython framebuffer bytearray to an I2C buffer, and write it to the device"
     for i, (x,y) in enumerate((x, y) for y in range(8) for x in range(8)):
         # extract a 16-bit pixel (x,y) from the specified framebuffer bytearray
-        n = 2*(x + width*y) # calculate the pixel address
+        n = 2*(x + x_offset + width*(y + y_offset)) # calculate the pixel address
         lo = fb[n]          # get the first (lo) byte
-        hi=fb[n+1]          # get the second (hi) byte
+        hi = fb[n+1]          # get the second (hi) byte
         p = lo + (hi << 8)  # combine the bytes into a 16-bit pixel
         # extract the RGB components from the 16-bit pixel
         red = p >> 11
@@ -35,7 +40,21 @@ def update(i2c, buff, fb, width=8):
     # send the bytearray to the I2C LED grid
     i2c.writeto_mem(0x46, 0, buf)
 
+#print (i2c.scan())
+
+#string = "Hello World!"
+string = "Hello World!"
+w = 8*(len(string) + 1)
+r = len(string) - 1
+
+fbuf, fb = create_framebuffer(width=w, height=16)
+
 fbuf.fill(0x2f << 11)
-fbuf.text('m',0, 0, 0xffff)
-update(i2c, buf, fb)
+fbuf.text(string,0, 0, 0xffff)
+
+for offset in range(8*len(string)):
+    update(i2c, buf, fb, x_offset=offset, y_offset=0, width=w)
+    time.sleep(0.1)
+
+    
 
