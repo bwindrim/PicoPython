52b85c6518e5ab776a9a1849d2c2db3de11ff375
Brian Windrim <bwindrim@gmail.com>
2023-07-31 12:20:30 +0100

    More simplification

--- SenseHat-1.py
+++ SenseHat-1.py
@@ -21,25 +21,23 @@ def create_framebuffer(width=8, height=8):
     fbuf = framebuf.FrameBuffer(fb, width, height, pix_format)
     return fbuf, fb
 
+# Rotation constants
 xf = [1, 24, -1, -24]
 yf = [24, -1, -24, 1]
 of = [0, 7, 7*25, 7*24]
 
 def update(i2c, buff, fb, x_offset=0, y_offset=0, width=8, dir=0):
     "Copy a MicroPython framebuffer bytearray to an I2C buffer, and write it to the device"
-    for i, (x,y) in enumerate((x, y) for y in range(8) for x in range(8)):
-        # extract a 16-bit pixel (x,y) from the specified framebuffer bytearray
-        n = 2*(x + x_offset + width*(y + y_offset)) # calculate the pixel address
-        p = fb[n] + (fb[n+1] << 8)  # combine the lo and hi bytes into a 16-bit pixel
-        # extract the RGB components from the 16-bit pixel
-        red = p >> 11
-        green = 0x2f & (p >> 5)
-        blue = p & 0x1f
-        # and write them (as 6-bit values) to an I2C bytearray
-        pos = of[dir] + xf[dir]*x + yf[dir]*y
-        buf[pos]      = red << 1
-        buf[pos + 8]  = green
-        buf[pos + 16] = blue << 1
+    for y in range(8):
+        for x in range(8):
+            # Extract a 16-bit pixel (x,y) from the specified framebuffer bytearray
+            n = 2*(x + x_offset + width*(y + y_offset)) # calculate the pixel address
+            p = fb[n] + (fb[n+1] << 8)  # combine the lo and hi bytes into a 16-bit pixel
+            # Write the RGB components from the 16-bit pixel (as 6-bit values) to an I2C bytearray
+            pos = of[dir] + xf[dir]*x + yf[dir]*y
+            buf[pos]      = (p >> 11) << 1
+            buf[pos + 8]  = 0x2f & (p >> 5)
+            buf[pos + 16] = (p & 0x1f) << 1
     # send the bytearray to the I2C LED grid
     i2c.writeto_mem(0x46, 0, buf)
 
