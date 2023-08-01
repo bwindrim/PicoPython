fda684a53d140a59e896e77cbadc0e76a1f64f86
Brian Windrim <bwindrim@gmail.com>
2023-08-01 17:42:10 +0100

    Incremental calculation of source pixel offset, using fewer multiplications

--- SenseHat-1.py
+++ SenseHat-1.py
@@ -21,16 +21,18 @@ of = [0, 7, 7*25, 7*24]
 def update(i2c, fb, x_offset=0, y_offset=0, width=8, dir=0):
     "Copy a MicroPython framebuffer bytearray to an I2C buffer, and write it to the device"
     buf = bytearray(192) # space for 8x8 LED array, 3 bytes per LED
+    n = 2*(x_offset + width*y_offset) # calculate the first pixel address
     for y in range(8):
         for x in range(8):
             # Extract a 16-bit pixel (x,y) from the specified framebuffer bytearray
-            n = 2*(x + x_offset + width*(y + y_offset)) # calculate the pixel address
-            p = fb[n] + (fb[n+1] << 8)  # combine the lo and hi bytes into a 16-bit pixel
+            pix = fb[n] + (fb[n+1] << 8)  # combine the lo and hi bytes into a 16-bit pixel
             # Write the RGB components from the 16-bit pixel (as 6-bit values) to an I2C bytearray
             pos = of[dir] + xf[dir]*x + yf[dir]*y
-            buf[pos]      = (p >> 11) << 1
-            buf[pos + 8]  = 0x2f & (p >> 5)
-            buf[pos + 16] = (p & 0x1f) << 1
+            buf[pos]      = (pix >> 11) << 1
+            buf[pos + 8]  = 0x2f & (pix >> 5)
+            buf[pos + 16] = (pix & 0x1f) << 1
+            n += 2
+        n += width + width - 16
     # send the bytearray to the I2C LED grid
     i2c.writeto_mem(0x46, 0, buf)
 
