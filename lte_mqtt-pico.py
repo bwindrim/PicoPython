0adedea2841ec797683490c224216e5e52e44e4d
Brian Windrim <bwindrim@gmail.com>
2025-07-15 15:16:45 +0100

    Cleanups:
    - removed netlight PWM class, LTE now just drives the LEB pin directly, at full brightness
    - now have separate topic names for publish and subscribe, even if they are the same
    - better handile of KeyboardInterrupt
    - some other reorgs and cleanups

--- lte_mqtt-pico.py
+++ lte_mqtt-pico.py
@@ -10,28 +10,24 @@ from machine import Pin, PWM, UART
 
 MOBILE_APN = "iot.1nce.net"
 
-# Fix the eye-searing brightness of the onboard LED with PWM
-class Netlight:
-    def __init__(self):
-        self.pin = PWM(Pin("LED", Pin.OUT), freq=1000)
-
-    def value(self, value):
-        self.pin.duty_u16(value * 2000)
-
-
-con = lte.LTE(MOBILE_APN, uart=UART(0, tx=Pin(16, Pin.OUT), rx=Pin(17, Pin.IN)), reset_pin=Pin(18, Pin.OUT), netlight_pin=Pin(19, Pin.IN), netlight_led=Netlight())
-con.start_ppp(baudrate=115200) # stay at the Clipper module's default baudrate
-
-
-# Change the following configs to suit your environment
-TOPIC           = 'BWtest/mqtt_async'
+# MQTT configuration
+# Note: broker.hivemq.com is a public broker, so no authentication is required.
+# If you want to use a private broker, you can set the username and password in the config.
+# For example:
+# config.username = 'your-username'
+# config.password = 'your-password'
+# The topic can be changed to whatever you want to publish/subscribe to.
+# The QoS level can also be adjusted as needed (0, 1, or 2).
+# The default QoS is 1, which means the message will be delivered at least once
+SUBSCRIBE_TOPIC = 'BWtest/mqtt_async'
+PUBLISH_TOPIC   = 'BWtest/mqtt_async'
 config.server   = 'broker.hivemq.com' # can also be a hostname
-#config.ssid     = 'wifi-ssid'
-#config.wifi_pw  = 'wifi-password'
 
-def callback(topic, msg, retained, qos): print(topic, msg, retained, qos)
+def callback(topic, msg, retained, qos):
+    print(topic, msg, retained, qos)
 
-async def conn_callback(client): await client.subscribe(TOPIC, 1)
+async def conn_callback(client):
+    await client.subscribe(SUBSCRIBE_TOPIC, 1)
 
 async def main(client):
     await client.connect()
@@ -39,19 +35,23 @@ async def main(client):
     n = 0
     while True:
         print('publish', n)
-        await client.publish(TOPIC, 'Hello World #{}!'.format(n), qos=1)
+        await client.publish(PUBLISH_TOPIC, f'Hello World #{n}!', qos=1)
         await asyncio.sleep(5)
         n += 1
 
 config.subs_cb = callback
 config.connect_coro = conn_callback
 
+con = lte.LTE(MOBILE_APN, uart=UART(0, tx=Pin(16, Pin.OUT), rx=Pin(17, Pin.IN)), reset_pin=Pin(18, Pin.OUT), netlight_pin=Pin(19, Pin.IN), netlight_led=Pin("LED", Pin.OUT))
+con.start_ppp(baudrate=115200) # stay at the Clipper module's default baudrate
+
 try:
     t_start = time.time()
     client = MQTTClient(config)
     loop = asyncio.get_event_loop()
     loop.run_until_complete(main(client))
-
+except KeyboardInterrupt:
+    print("KeyboardInterrupt: stopping...")
 finally:
     t_end = time.time()
     print(f"Took: {t_end - t_start} seconds")
